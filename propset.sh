7fb9fd16002a5ef229b8c23238cc66537997116b
n3x15 <n3x15@97ece4ad-cae2-4924-842c-0ef94757c1ff>
2009-06-20 23:51:38 +0000

    Props.
    git-svn-id: file:///home/nexis/Documents/luna-mirror/trunk@15 97ece4ad-cae2-4924-842c-0ef94757c1ff

---
+++ propset.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Needs to be done, or loading the client WILL fail.
+#
+find . -type f -name "*.h" -print | while 
+read filename 
+do ( 
+	echo $filename;
+	flip -u $filename
+	svn propset svn:eol-style native $filename
+) 
+done
+find . -type f -name "*.cpp" -print | while
+read filename
+do (
+        echo $filename;
+        flip -u $filename
+        svn propset svn:eol-style native $filename     
+)
+done
+find . -type f -name "*.xml" -print | while
+read filename
+do (
+        echo $filename;
+        flip -u $filename
+        svn propset svn:eol-style native $filename
+)
+done
+find . -type f -name "*.txt" -print | while
+read filename
+do (
+        echo $filename;
+        flip -u $filename
+        svn propset svn:eol-style native $filename
+)
+done
+find . -type f -name "*.ini" -print | while
+read filename
+do (
+        echo $filename;
+        flip -u $filename
+        svn propset svn:eol-style native $filename
+)
+done
