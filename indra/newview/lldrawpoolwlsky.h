e1e0e418a7e01f669ee3128c996f59fea1d1ac5d
n3x15 <n3x15@97ece4ad-cae2-4924-842c-0ef94757c1ff>
2009-09-13 05:49:15 +0000

    Whoops
    git-svn-id: file:///home/nexis/Documents/luna-mirror/trunk@104 97ece4ad-cae2-4924-842c-0ef94757c1ff

--- indra/newview/lldrawpoolwlsky.h
+++ indra/newview/lldrawpoolwlsky.h
@@ -89,7 +89,7 @@ private:
 	void SetupCumulusMediocrisClouds();
 
 private:
-	static Atmosphere * atm = 0;	// The Atmosphere object is the main interface to SilverLining.
+	static SilverLining::Atmosphere * atm = 0;	// The Atmosphere object is the main interface to SilverLining.
 };
 
 #endif // LL_DRAWPOOLWLSKY_H
