00a4435059f7961d86623d4011f438227f4a943a
n3x15 <n3x15@97ece4ad-cae2-4924-842c-0ef94757c1ff>
2010-02-21 05:21:49 +0000

    FixSVN.lua - For lazy fucks like me :>
    git-svn-id: file:///home/nexis/Documents/luna-mirror/trunk@216 97ece4ad-cae2-4924-842c-0ef94757c1ff

---
+++ scripts/FixSVN.lua
@@ -0,0 +1,34 @@
+--[[ written for Lua 5.1
+split a string by a pattern, take care to create the "inverse" pattern 
+yourself. default pattern splits by white space.
+]]
+string.split = function(str, pattern)
+  pattern = pattern or "[^%s]+"
+  if pattern:len() == 0 then pattern = "[^%s]+" end
+  local parts = {__index = table.insert}
+  setmetatable(parts, parts)
+  str:gsub(pattern, parts)
+  setmetatable(parts, nil)
+  parts.__index = nil
+  return parts
+end
+
+for line in io.lines() do
+	local chunks = string.split(line,"[^%s]+")
+	local c = 2
+	local filename = chunks[2]
+	if (#chunks>2) then
+		while c<#chunks+1 do
+			filename = filename.."\\ "..chunks[c]
+			c=c+1
+		end
+	end
+	if chunks[1]=="!" then
+		print ("DELETE",filename)
+		os.execute("svn delete "..filename)
+	elseif chunks[1]=="?" then
+		print ("ADD",filename)
+		os.execute("svn add "..filename)
+	end
+end
+
