90466178e218ac96e8f0a4b2b3093d2c0c7434fe
unknown <nexisentertainment@gmail.com>
2010-09-19 19:17:43 -0700

    glib is AFU on linux

--- indra/cmake/UI.cmake
+++ indra/cmake/UI.cmake
@@ -1,10 +1,9 @@
 # -*- cmake -*-
 include(Prebuilt)
 
-if (STANDALONE)
+if (LINUX)
   include(FindPkgConfig)
     
-  if (LINUX)
     set(PKGCONFIG_PACKAGES
         atk
         cairo
@@ -21,7 +20,6 @@ if (STANDALONE)
         pangoxft
         sdl
         )
-  endif (LINUX)
 
   foreach(pkg ${PKGCONFIG_PACKAGES})
     pkg_check_modules(${pkg} REQUIRED ${pkg})
@@ -30,28 +28,10 @@ if (STANDALONE)
     list(APPEND UI_LIBRARIES ${${pkg}_LIBRARIES})
     add_definitions(${${pkg}_CFLAGS_OTHERS})
   endforeach(pkg)
-else (STANDALONE)
+else (LINUX)
   if (NOT DARWIN)
     use_prebuilt_binary(gtk-atk-pango-glib)
   endif (NOT DARWIN)
-  if (LINUX)
-    set(UI_LIBRARIES
-        atk-1.0
-        gdk-x11-2.0
-        gdk_pixbuf-2.0
-        Xinerama
-        glib-2.0
-        gmodule-2.0
-        gobject-2.0
-        gthread-2.0
-        gtk-x11-2.0
-        pango-1.0
-        pangoft2-1.0
-        pangox-1.0
-        pangoxft-1.0
-        )
-  endif (LINUX)
-
   include_directories (
       ${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include
       ${LIBS_PREBUILT_DIR}/include
@@ -59,7 +39,7 @@ else (STANDALONE)
   foreach(include ${${LL_ARCH}_INCLUDES})
       include_directories(${LIBS_PREBUILT_DIR}/${LL_ARCH_DIR}/include/${include})
   endforeach(include)
-endif (STANDALONE)
+endif (LINUX)
 
 if (LINUX)
   add_definitions(-DLL_GTK=1 -DLL_X11=1)
