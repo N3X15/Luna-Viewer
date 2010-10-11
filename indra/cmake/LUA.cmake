081fef11a7c461134f9b5a0367f4512c93774e48
N3X15 <nexisentertainment@gmail.com>
2010-10-11 02:20:06 -0700

    Fix for Morgaine.

--- indra/cmake/LUA.cmake
+++ indra/cmake/LUA.cmake
@@ -1,11 +1,11 @@
 # -*- cmake -*-
 include(Prebuilt)
-find_package(liblua REQUIRED)
+find_package(Lua51)
 
-IF (LibLua_FOUND)
-	set(LUA_LIBRARY ${LibLua_LIBRARIES})
-	set(LUA_INCLUDES ${LibLua_INCLUDE_DIRS})
-ELSE (LibLua_FOUND)
+IF (LUA51_FOUND)
+	set(LUA_LIBRARY ${LUA_LIBRARY})
+	set(LUA_INCLUDES ${LUA_INCLUDE_DIR})
+ELSE (LUA51_FOUND)
 	#set(LUA_LIBRARY liblua5.1.so)
 	set(LUA_LIBRARY lua5.1)
-ENDIF (LibLua_FOUND)
+ENDIF (LUA51_FOUND)
