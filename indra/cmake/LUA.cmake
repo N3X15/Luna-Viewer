0ec4b0766c26b31d68b7e541c59100973fe2bca4
N3X15 <nexisentertainment@gmail.com>
2010-10-10 23:37:32 -0700

    Don't know if this will work or not.

--- indra/cmake/LUA.cmake
+++ indra/cmake/LUA.cmake
@@ -1,5 +1,11 @@
 # -*- cmake -*-
 include(Prebuilt)
+find_package(liblua REQUIRED)
 
-#set(LUA_LIBRARY liblua5.1.so)
-set(LUA_LIBRARY lua5.1)
+IF (LibLua_FOUND)
+	set(LUA_LIBRARY ${LibLua_LIBRARIES})
+	set(LUA_INCLUDES ${LibLua_INCLUDE_DIRS})
+ELSE (LibLua_FOUND)
+	#set(LUA_LIBRARY liblua5.1.so)
+	set(LUA_LIBRARY lua5.1)
+ENDIF (LibLua_FOUND)
---
+++ indra/newview/lua/Hooks/AO/_init_.lua
@@ -0,0 +1,41 @@
+--[[
+Blah blah blah GPLv2, I don't care.
+
+Rob N3X15 Nelson
+]]--
+
+-- Standing animations.
+AO={}
+AO.AnimationStates={
+	["Walking"]={
+		"2408fe9e-df1d-1d7d-f4ff-1384fa7b350f",
+		"15468e00-3400-bb66-cecc-646d7c14458e",
+		"370f3a20-6ca6-9971-848c-9a01bc42ae3c",
+		"42b46214-4b44-79ae-deb8-0df61424ff4b",
+		"f22fed8b-a5ed-2c93-64d5-bdd8b93c889f"
+	},
+	["Voice"]={
+		"3557510a-5eb4-d0ce-0b91-67c72aa75312",
+    	"a71890f1-0dab-8744-fd47-7defaf411dbf",
+    	"c1802201-5f4e-366f-7f78-2d08ec6ea54a",
+    	"68db359f-4c9c-0932-5f1e-e95e3a0b19bc",
+    	"7ef0d5c0-3346-06e4-5cfc-f081db108baa",
+    	"28a3f544-268d-da71-7da6-82c8dd522cb9",
+    	"cc340155-3e9d-60fe-d8e3-9e9abc7062d1",
+    	"55fe6788-8a16-d998-2f63-3c1eab2b6009",
+    	"69d5a8ed-9ec6-6dac-842f-d92d82e69428",
+    	"9a7f3201-7bbd-4f75-b762-24270536e4e3",
+    	"37694185-3107-d418-3a20-0181424e542d",
+    	"cb1139b6-e7c3-fdc7-a9c1-e21673d7a50e",
+    	"bbf194d1-a118-1312-998b-8145cec6eaff",
+    	"593e9a3d-58d8-c594-d6dd-f4b98965202e",
+    	"2b78c24a-2451-6135-fc49-ad274552bb68",
+    	"0f645c60-3151-2805-b6f7-28e710ed22ac"
+	}
+}
+
+AO.AnimationOverrides={}
+
+function AO:ChangeStands()
+	print "Durp"
+end
