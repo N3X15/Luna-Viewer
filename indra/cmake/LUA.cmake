3ff783eb8685cd7a1df67993b52322e2bf7374a5
N3X15 <nexisentertainment@gmail.com>
2010-10-17 15:36:44 -0700

    Integrated Lua

--- indra/cmake/LUA.cmake
+++ indra/cmake/LUA.cmake
@@ -1,11 +1,14 @@
 # -*- cmake -*-
-include(Prebuilt)
-find_package(Lua51)
+#include(Prebuilt)
+#find_package(Lua51)
+#
+#IF (LUA51_FOUND)
+#	set(LUA_LIBRARY ${LUA_LIBRARY})
+#	set(LUA_INCLUDES ${LUA_INCLUDE_DIR})
+#ELSE (LUA51_FOUND)
+#	#set(LUA_LIBRARY liblua5.1.so)
+#	set(LUA_LIBRARY lua5.1)
+#ENDIF (LUA51_FOUND)
 
-IF (LUA51_FOUND)
-	set(LUA_LIBRARY ${LUA_LIBRARY})
-	set(LUA_INCLUDES ${LUA_INCLUDE_DIR})
-ELSE (LUA51_FOUND)
-	#set(LUA_LIBRARY liblua5.1.so)
-	set(LUA_LIBRARY lua5.1)
-ENDIF (LUA51_FOUND)
+set (LUA_LIBRARY lua5.1)
+set (LUA_INCLUDE_DIR ${LIBS_OPEN_DIR}/lua/src)
