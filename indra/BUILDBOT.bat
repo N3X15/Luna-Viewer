ef6eaaebfe23c6f2ccdc2f031a5bf072ed8f92b0
N3X15 <nexisentertainment@gmail.com>
2010-10-18 18:50:35 -0700

    Buildscript fixes for Buildbot again, ensuring we get the full rainbow of working builds.

--- indra/BUILDBOT.bat
+++ indra/BUILDBOT.bat
@@ -1,20 +1,4 @@
-::@echo off
 @rem %1 = VC version to use (VS2005/VS2008*/VS2010)
-
-@rem Dick with settings
-call ..\scripts\automated_build_scripts\update_settings.bat
-
-@rem Clean installables cache
-rm ../installed.xml
-
-@rem Set your build type here.  Release = no debugging symbols.  RelWithDebInfo = Optimized with debugging symbols. Debug = FAT AND SLOW, MUCH LIKE YOUR MOTHER
-set BUILDTYPE=RelWithDebInfo
-
-@rem TODO: Turn this into a horrific python monstrosity.
-@rem	Gather hooks in order to generate documentation.
-@rem	Will fail without cygwin, so find a better way.
-@rem find newview/ -name "*.cpp" -print | C:\lua5.1\lua ../scripts/GetHooks.lua
-
 @rem set GEN=NMake Makefiles
 if /i %1 == vs2005		set GEN=Visual Studio 8 2005
 if /i %1 == vs2005-x64	set GEN=Visual Studio 8 2005 Win64
@@ -23,6 +7,22 @@ if /i %1 == vs2008-x64	set GEN=Visual Studio 9 2008 Win64
 if /i %1 == vs2010		set GEN=Visual Studio 10
 if /i %1 == vs2010-x64	set GEN=Visual Studio 10 Win64
 
+@rem Dick with settings
+call ..\scripts\automated_build_scripts\update_settings.bat
+
+@rem Clean installables cache
+rm ../installed.xml
+
+@rem Clean CMake cache
+rm CMakeCache*
+
+@rem Set your build type here.  Release = no debugging symbols.  RelWithDebInfo = Optimized with debugging symbols. Debug = FAT AND SLOW, MUCH LIKE YOUR MOTHER
+set BUILDTYPE=RelWithDebInfo
+
+@rem TODO: Turn this into a horrific python monstrosity.
+@rem	Gather hooks in order to generate documentation.
+@rem	Will fail without cygwin, so find a better way.
+@rem find newview/ -name "*.cpp" -print | C:\lua5.1\lua ../scripts/GetHooks.lua
 
 @rem Prep for nmake horseassery.
 cmake -G "%GEN%" -DUNATTENDED:BOOL=OFF -DSTANDALONE:BOOL=OFF -DWORD_SIZE:STRING=32 -DOPENSIM_RULES:BOOL=OFF -DROOT_PROJECT_NAME:STRING=Luna .
